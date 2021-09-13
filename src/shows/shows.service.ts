0dfbbe864f32e7681abce0c7656c949f1906f2e4
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2021-09-13 12:23:16 -0300

    Add logs for every operation.

--- src/shows/shows.service.ts
+++ src/shows/shows.service.ts
@@ -10,30 +10,39 @@ export class ShowsService {
   constructor(@InjectModel(Show.name) private showModel: Model<ShowDocument>) { }
 
   async findAll() {
+    Logger.log('Searching for all shows...');
     return await this.showModel.find().exec();
   }
 
   async findOne(id: string) {
+    Logger.log(`Searching for shows - id ${id}`);
     return await this.showModel.findById(id);
   }
 
   async findRandom() {
+    Logger.log('Selecting random show...');
+
     const all = await this.showModel.find().exec();
     return all[Math.floor(Math.random() * all.length)];
   }
 
   async create(createShowDto: CreateShowDto): Promise<ShowDocument> {
+    Logger.log('Creating new show...', JSON.stringify(createShowDto));
+
     const createdShow = new this.showModel(createShowDto);
     return await createdShow.save();
   }
 
   async update(id: string, show: Show): Promise<ShowDocument> {
+    Logger.log(`Updating show - id ${id}`);
+
     await this.showModel.updateOne({ _id: id }, show).exec();
 
     return this.showModel.findById(id);
   }
 
   async remove(id: string): Promise<void> {
+    Logger.log(`Removing show - id ${id}`);
     await this.showModel.deleteOne({ _id: id });
   }
 }
